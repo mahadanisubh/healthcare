adb3722ec241d1695ae729b24459a45716c7551c
Subha Mahadani <mahadanisubh@gmail.com>
2026-03-28 11:49:12 +0530

    backend origin changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,10 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+   origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 
 import requestRoutes from "./routes/request.routes.js";
